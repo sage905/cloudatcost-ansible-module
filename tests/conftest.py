209d09735f9ae4bd9b8c93849be590f5d942f658
Patrick Toal <ptoal@takeflight.ca>
2017-02-22 11:59:39 -0500

    Alternate test methods

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,25 +1,20 @@
 from cacpy.CACPy import *
 import pytest
 
-ROOT_URL = BASE_URL + API_VERSION
+V1_SERVER_DEFAULT = {'sdate': '07/14/2015', 'uid': '4482712345', 'ip': '10.1.1.2',
+                     'servername': 'c123456789-cloudpro-123456789', 'ram': '2048', 'portgroup': 'Cloud-ip-123',
+                     'id': '123456789', 'label': 'serverlabel', 'vmname': 'c90000-CloudPRO-123456789-123456789',
+                     'gateway': '10.1.1.1', 'hdusage': '5.123456789', 'rdns': 'server.test.example',
+                     'rootpass': 'password',
+                     'vncport': '12345', 'hostname': 'server.test.example', 'storage': '10', 'cpuusage': '26',
+                     'template': 'CentOS-7-64bit', 'sid': '123456789', 'vncpass': 'secret',
+                     'status': 'Powered On',
+                     'lable': 'serverlabel', 'servertype': 'cloudpro',
+                     'rdnsdefault': 'notassigned.cloudatcost.com',
+                     'netmask': '255.255.255.0', 'ramusage': '763.086', 'mode': 'Normal', 'packageid': '15',
+                     'panel_note': 'testnote', 'cpu': '4'}
 
-V1_LISTSERVERS_RESPONSE = {'status': 'ok', 'action': 'listservers', 'api': 'v1',
-                           'data': [{'sdate': '07/14/2015', 'uid': '4482712345', 'ip': '10.1.1.2',
-                                    'servername': 'c123456789-cloudpro-123456789', 'ram': '2048',
-                                    'portgroup': 'Cloud-ip-123',
-                                    'id': '123456789', 'label': 'serverlabel',
-                                    'vmname': 'c90000-CloudPRO-123456789-123456789',
-                                    'gateway': '10.1.1.1', 'hdusage': '5.123456789', 'rdns': 'server.test.example',
-                                    'rootpass': 'password',
-                                    'vncport': '12345', 'hostname': 'server.test.example', 'storage': '10',
-                                    'cpuusage': '26',
-                                    'template': 'CentOS-7-64bit', 'sid': '123456789', 'vncpass': 'secret',
-                                    'status': 'Powered On',
-                                    'lable': 'serverlabel', 'servertype': 'cloudpro',
-                                    'rdnsdefault': 'notassigned.cloudatcost.com',
-                                    'netmask': '255.255.255.0', 'ramusage': '763.086', 'mode': 'Normal',
-                                    'packageid': '15',
-                                    'panel_note': 'testnote', 'cpu': '4'}], 'time': 1487000464}
+V1_LISTSERVERS_RESPONSE = {'status': 'ok', 'action': 'listservers', 'api': 'v1', 'time': 1487000464}
 
 V1_LIST_TEMPLATES_RESPONSE = {'status': 'ok', 'action': 'listtemplates', 'api': 'v1',
                               'data': [{'ce_id': '1', 'name': 'CentOS 6.7 64bit'},
@@ -38,12 +33,32 @@ V1_STANDARD_RESPONSE_OK = {
     "data": []
 }
 
+ROOT_URL = BASE_URL + API_VERSION
+
 V1_STANDARD_RESPONSE_ERROR = {
     "status": "error",
     "time": 1425064819,
     "error": 104,
     "error_description": "Error Test Response"
 }
+
+
+class MockServer(object):
+    def __init__(self):
+        self.server = V1_SERVER_DEFAULT.copy()
+
+    def get_server(self):
+        return self.server
+
+    def set_server_attribute(self, attribute, value):
+        self.server[attribute] = value
+
+    def get_server_as_list(self):
+        return [self.server, ]
+
+
+def merge_dicts(*dicts):
+    return reduce(lambda x, y: x.update(y) or x, dicts, {})
 
 
 def mocked_requests_get(*args, **kwargs):
@@ -57,7 +72,7 @@ def mocked_requests_get(*args, **kwargs):
 
     return {
         ROOT_URL + LIST_SERVERS_URL:
-            MockResponse(V1_LISTSERVERS_RESPONSE, 200),
+            MockResponse(merge_dicts(V1_LISTSERVERS_RESPONSE, {'data': MockServer().get_server_as_list()}), 200),
         ROOT_URL + LIST_TEMPLATES_URL:
             MockResponse(V1_LIST_TEMPLATES_RESPONSE, 200),
     }.get(args[0], MockResponse(V1_STANDARD_RESPONSE_ERROR, 404))
